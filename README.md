15f96150918833f06fa53bcc4c4d5214be3f2c05
Apoorv Vardhan <vardhanapoorv@gmail.com>
2020-04-28 21:41:46 +0530

    Updated README file

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ yarn start
 ### Adding a page 
 
 Just add a `mdx` file with the `title` on top of the file. Now if you want to add a section which can have more parts then just create a folder and add the `index.mdx` file for the main file and again add a `title` field on top and then add as many files you want `*.mdx`.
+
+### Adding GraphiQL component
+
+The common GraphiQL component can be used throughout the site, just include it in the `mdx` file you want and you can even pass a `query` to fill it with a default value. Refer `index.js` for basic usage and  `api.mdx` for including a default value.
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,33 +5,10 @@ import "graphiql/graphiql.css"
 import SEO from "../components/seo"
 import GraphiQLWrapper from "../components/GraphiQL"
 
-const query = `{
-  queryUser {
-    id
-    firstName
-    email
-    orgs {
-      org {
-        name
-        id
-        users {
-          role
-          user {
-            firstName
-            email
-          }
-        }
-      }
-    }
-  }
-}`
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-      <GraphiQLWrapper defaultQuery={query} />
-    {/* <Provider store={store}>
-      <Playground endpoint="http://localhost:8080/graphql" createApolloLink={graphQLFetcher}/>
-    </Provider> */}
+    <GraphiQLWrapper />
   </Layout>
 )
 
