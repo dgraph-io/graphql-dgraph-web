9411b8cd5b19f51a5fa0a99bebc8ebec9e416ab8
shrutika <shrutika@geekyants.com>
2020-06-30 18:22:32 +0530

    remove slack

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaTwitter, FaGithub, FaSlack } from "react-icons/fa";
+import { FaTwitter, FaGithub } from "react-icons/fa";
 
 export default (props) => {
   return (
