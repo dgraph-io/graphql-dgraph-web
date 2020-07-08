a2cc5a8935bd95b942332578bc13f50a0ada732e
Debabrata <debabratab@geekyants.com>
2020-07-08 16:56:05 +0530

    Feat(editor):code editor implemented

---
+++ src/pages/dgraphGraphQL/editor.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import GraphiQLWrapper from '../../components/GraphiQL';
+
+export default function CodeEditor()
+{
+    return (
+        <>
+            <GraphiQLWrapper />
+        </>
+    )
+}
