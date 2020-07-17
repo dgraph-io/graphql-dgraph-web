93f50d91ffd27ecaae63034ee9b51ebe4f242270
Debabrata <debabratab@geekyants.com>
2020-07-17 10:35:28 +0530

    yarn clean

--- public/v20.03.1/252f366e-d8cdc904de64ed4601c4.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{NIcq:function(t,n,a){"use strict";a.d(n,"a",(function(){return c}));var e=a("Lnxd"),c=function(t){return Object(e.a)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"}}]})(t)};c.displayName="MdChevronLeft"}}]);
-//# sourceMappingURL=252f366e-d8cdc904de64ed4601c4.js.map
