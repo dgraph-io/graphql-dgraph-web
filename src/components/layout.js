26c9b0bd21f0e732ebaec79d1db6713d1332355c
Debabrata <debabratab@geekyants.com>
2020-08-19 16:42:56 +0530

    implemented adroll

--- src/components/layout.js
+++ src/components/layout.js
@@ -51,7 +51,7 @@ const Layout = props => {
       `}
       render={data => (
         <>
-        {/* <Helmet>
+          {/* <Helmet>
           <script src="./docSearch.js" type="text/javascript" />
         </Helmet> */}
           <SEO
@@ -72,8 +72,41 @@ const Layout = props => {
             }}
           </Location>
           <Helmet>
-            <script  src="../assets/adroll.js"/> 
-  
+            <script type="text/javascript">
+              {`
+                    adroll_adv_id = "L42C3U2UXBBPVAMISVXVLC"
+                    adroll_pix_id = "MOR4VUNPK5B2LHEZ2NNTDX"
+                    adroll_version = "2.0"
+                    console.log("inadroll")
+                    ;(function(w, d, e, o, a) {
+                      w.__adroll_loaded = true
+                      w.adroll = w.adroll || []
+                      w.adroll.f = ["setProperties", "identify", "track"]
+                      var roundtripUrl =
+                        "https://s.adroll.com/j/" + adroll_adv_id + "/roundtrip.js"
+                      for (a = 0; a < w.adroll.f.length; a++) {
+                        w.adroll[w.adroll.f[a]] =
+                          w.adroll[w.adroll.f[a]] ||
+                          (function(n) {
+                            return function() {
+                              w.adroll.push([n, arguments])
+                            }
+                          })(w.adroll.f[a])
+                      }
+                      e = d.createElement("script")
+                      o = d.getElementsByTagName("script")[0]
+                      e.async = 1
+                      e.src = roundtripUrl
+                      o.parentNode.insertBefore(e, o)
+                    })(window, document)
+                    adroll.track("pageView")
+
+                `}
+            </script>
+            {console.log(
+              "after scipt",
+              document.getElementsByTagName("head")[0].innerHTML
+            )}
           </Helmet>
           <div
             className="sidebar-container-visible"
@@ -85,7 +118,7 @@ const Layout = props => {
               selectSideBarContent={(nodeTitle, contentClass) => {
                 setContentCategory(nodeTitle, contentClass)
               }}
-              sidebarcategoryindex={sideBarCategoryIndex}  
+              sidebarcategoryindex={sideBarCategoryIndex}
               showSideBar={isSideBarVisible => showSideBar(isSideBarVisible)}
             />
           </div>
@@ -120,8 +153,8 @@ const Layout = props => {
                 getSideBarClass(props)
                   ? "landing-pg-extend-sidebar pl-lg-5 pl-md-5"
                   : renderRightSideBar
-                  ? "landing-pg  pl-lg-5 pl-md-5 "
-                  : "landing-pg-extend pl-lg-5 pl-md-5"
+                    ? "landing-pg  pl-lg-5 pl-md-5 "
+                    : "landing-pg-extend pl-lg-5 pl-md-5"
               }
             >
               {props.children}
